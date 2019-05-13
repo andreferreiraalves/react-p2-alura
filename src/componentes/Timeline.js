07bbe47f6c19c507ba20f105b59fff5cce7b1be4
andre_ferreira_app <andre.ferreira@mercatustecnologia.com.br>
2019-05-12 23:12:52 -0300

    login

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -9,17 +9,21 @@ export default class Timeline extends Component {
 
     componentDidMount() {
         fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
-            .then(response => response.json())
-            .then(fotos => {
-                this.setState({ fotos: fotos });
-            });
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('problemas para obter as fotos');
+                }
+            })
+            .then(fotos => this.setState({ fotos }));
     }
 
     render() {
         return (
             <div className="fotos container">
                 {
-                    this.state.fotos.map(foto => <FotoItem foto={foto} />)
+                    this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} />)
                 }
             </div>
         );
